89d7b5957111d913e13274c16cabf51c857e49a6
Piotr Mulinski <piotr.mulinski@icloud.com>
2026-04-06 21:55:38 +0200

    migration: AddRestrictPartSupplier

--- API_Minimal_Project_PM/Eindpoints/CategoryEndpoints.cs
+++ API_Minimal_Project_PM/Eindpoints/CategoryEndpoints.cs
@@ -10,59 +10,59 @@ namespace API_Minimal_Project_PM.Eindpoints
         {
             var categoryGroup = app.MapGroup("/api/Category").WithTags("Category");
 
-            // 1. GET: GetAllCategories
-            categoryGroup.MapGet("/", async Task<Results<NotFound, Ok<IEnumerable<Category>>>> (ICategoryRepository repo) =>
-            {
-                var result = await repo.GetAllCategories();
-                if (!result.Any()) return TypedResults.NotFound();
+            //    // 1. GET: GetAllCategories
+            //    categoryGroup.MapGet("/", async Task<Results<NotFound, Ok<IEnumerable<Category>>>> (ICategoryRepository repo) =>
+            //    {
+            //        var result = await repo.GetAllCategories();
+            //        if (!result.Any()) return TypedResults.NotFound();
 
-                return TypedResults.Ok(result);
-            })
-            .Produces(StatusCodes.Status500InternalServerError);
+            //        return TypedResults.Ok(result);
+            //    })
+            //    .Produces(StatusCodes.Status500InternalServerError);
 
-            // 2. GET: GetCategoryById
-            categoryGroup.MapGet("/{id:int}", async Task<Results<NotFound, Ok<Category>>> (int id, ICategoryRepository repo) =>
-            {
-                var result = await repo.GetCategoryById(id);
-                if (result is null) return TypedResults.NotFound();
+            //    // 2. GET: GetCategoryById
+            //    categoryGroup.MapGet("/{id:int}", async Task<Results<NotFound, Ok<Category>>> (int id, ICategoryRepository repo) =>
+            //    {
+            //        var result = await repo.GetCategoryById(id);
+            //        if (result is null) return TypedResults.NotFound();
 
-                return TypedResults.Ok(result);
-            })
-            .WithName("GetCategoryById")
-            .Produces(StatusCodes.Status500InternalServerError);
+            //        return TypedResults.Ok(result);
+            //    })
+            //    .WithName("GetCategoryById")
+            //    .Produces(StatusCodes.Status500InternalServerError);
 
-            // 3. POST: CreateCategory
-            categoryGroup.MapPost("/", async Task<Results<BadRequest, CreatedAtRoute<Category>>> (Category item, ICategoryRepository repo) =>
-            {
-                if (item is null) return TypedResults.BadRequest();
+            //    // 3. POST: CreateCategory
+            //    categoryGroup.MapPost("/", async Task<Results<BadRequest, CreatedAtRoute<Category>>> (Category item, ICategoryRepository repo) =>
+            //    {
+            //        if (item is null) return TypedResults.BadRequest();
 
-                await repo.CreateCategory(item);
+            //        await repo.CreateCategory(item);
 
-                return TypedResults.CreatedAtRoute(item, "GetCategoryById", new { id = item.Id });
-            })
-            .Produces(StatusCodes.Status500InternalServerError);
+            //        return TypedResults.CreatedAtRoute(item, "GetCategoryById", new { id = item.Id });
+            //    })
+            //    .Produces(StatusCodes.Status500InternalServerError);
 
-            // 4. PUT: UpdateCategory
-            categoryGroup.MapPut("/{id:int}", async Task<Results<BadRequest, NotFound, NoContent>> (int id, Category item, ICategoryRepository repo) =>
-            {
-                if (item is null || id != item.Id) return TypedResults.BadRequest();
+            //    // 4. PUT: UpdateCategory
+            //    categoryGroup.MapPut("/{id:int}", async Task<Results<BadRequest, NotFound, NoContent>> (int id, Category item, ICategoryRepository repo) =>
+            //    {
+            //        if (item is null || id != item.Id) return TypedResults.BadRequest();
 
-                bool updated = await repo.UpdateCategory(id, item);
-                if (!updated) return TypedResults.NotFound();
+            //        bool updated = await repo.UpdateCategory(id, item);
+            //        if (!updated) return TypedResults.NotFound();
 
-                return TypedResults.NoContent();
-            })
-            .Produces(StatusCodes.Status500InternalServerError);
+            //        return TypedResults.NoContent();
+            //    })
+            //    .Produces(StatusCodes.Status500InternalServerError);
 
-            // 5. DELETE: DeleteCategory
-            categoryGroup.MapDelete("/{id:int}", async Task<Results<NotFound, NoContent>> (int id, ICategoryRepository repo) =>
-            {
-                bool deleted = await repo.DeleteCategory(id);
-                if (!deleted) return TypedResults.NotFound();
+            //    // 5. DELETE: DeleteCategory
+            //    categoryGroup.MapDelete("/{id:int}", async Task<Results<NotFound, NoContent>> (int id, ICategoryRepository repo) =>
+            //    {
+            //        bool deleted = await repo.DeleteCategory(id);
+            //        if (!deleted) return TypedResults.NotFound();
 
-                return TypedResults.NoContent();
-            })
-            .Produces(StatusCodes.Status500InternalServerError);
+            //        return TypedResults.NoContent();
+            //    })
+            //    .Produces(StatusCodes.Status500InternalServerError);
         }
 
     }
---
+++ API_Project_PM.Core/Migrations/20260406195049_AddRestrictPartSupplier.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Project_PM.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRestrictPartSupplier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PartSuppliers_Parts_PartId",
+                table: "PartSuppliers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PartSuppliers_Parts_PartId",
+                table: "PartSuppliers",
+                column: "PartId",
+                principalTable: "Parts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PartSuppliers_Parts_PartId",
+                table: "PartSuppliers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PartSuppliers_Parts_PartId",
+                table: "PartSuppliers",
+                column: "PartId",
+                principalTable: "Parts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
