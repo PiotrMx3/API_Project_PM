4ebe90ee48facee83a14f903b201cdc7a3747156
Piotr Mulinski <piotr.mulinski@icloud.com>
2026-04-17 12:57:45 +0200

    migrations: update

---
+++ API_Project_PM.Core/Migrations/20260417103415_AddRestrictPartLocation.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Project_PM.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRestrictPartLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Parts_Locations_DefaultLocationId",
+                table: "Parts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Parts_Locations_DefaultLocationId",
+                table: "Parts",
+                column: "DefaultLocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Parts_Locations_DefaultLocationId",
+                table: "Parts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Parts_Locations_DefaultLocationId",
+                table: "Parts",
+                column: "DefaultLocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
